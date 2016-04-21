8843c2710cebf9f5cf2ce4d990cdbec17ad10abf
alexhickman <alex.hickman3@gmail.com>
2016-04-21 15:37:37 -0700

    Initial Commit

---
+++ PhotoBombers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoBombers
+//
+//  Created by Hickman on 4/21/16.
+//  Copyright © 2016 Hickman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoBombers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoBombers
+//
+//  Created by Hickman on 4/21/16.
+//  Copyright © 2016 Hickman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
